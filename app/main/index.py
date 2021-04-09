2142f5d30e862817cb174d9cfed323de153976e9
jungking <immenige2@gmail.com>
2021-04-09 20:43:25 +0900

    .

--- app/main/index.py
+++ app/main/index.py
@@ -3,26 +3,26 @@
 from flask import Blueprint, request, render_template, flash, redirect, url_for
 from flask import current_app as app
 
-app = Blueprint('main',__name__, url_prefix='/')
+main = Blueprint('main',__name__, url_prefix='/')
 
-@app.route('/') # /main 으로하면 127.0.0.1:3000/main으로 가야 입력 됨.
+@main.route('/') # /main 으로하면 127.0.0.1:3000/main으로 가야 입력 됨.
 def index():
     testData = 'testData array'
     return render_template('main/index.html', testDataHtml = testData)
 
-@app.route('/profile')
+@main.route('/profile')
 def profile():
     return render_template('/profile.html')
 
-@app.route('/date<int:num>')
+@main.route('/date<int:num>')
 def datecal(num=None):
     return render_template('/date.html', num=num)
 
-@app.route('/date')
+@main.route('/date')
 def datecal1(num=None):
     return render_template('/date.html', num=num)    
 
-@app.route('/date/calculate',methods=['POST'])
+@main.route('/date/calculate',methods=['POST'])
 def calculate(num=None):
     if request.method == 'POST':
         temp = request.form['num']
