fa8b59c8db303cba64390bd5af50d546dd1d4979
jungking <immenige2@gmail.com>
2021-04-10 14:34:39 +0900

    append : remark

--- app/main/index.py
+++ app/main/index.py
@@ -10,27 +10,38 @@ def index():
     testData = 'testData array'
     return render_template('main/index.html', testDataHtml = testData)
 
-@main.route('/profile')
+@main.route('/profile') #프로필 창 들어가기
 def profile():
     return render_template('/profile.html')
 
-@main.route('/date<int:num>')
+@main.route('/upload<int:num>') #업로드 창 int값
 def datecal(num=None):
-    return render_template('/date.html', num=num)
+    return render_template('/upload.html', num=num)
 
-@main.route('/date')
+@main.route('/upload') #업로드 창 들어가기
 def datecal1(num=None):
-    return render_template('/date.html', num=num)    
+    return render_template('/upload.html', num=num)    
 
-@main.route('/picture')
+@main.route('/picture') #사진 창 들어가기
 def picture():
     return render_template('/picture.html')
 
-@main.route('/date/calculate',methods=['POST'])
+@main.route('/picture/prev',methods=['POST']) #프로필탭 이전사진으로
+def prev():
+    return render_template('/picture.html')
+
+@main.route('/picture/next',methods=['POST']) #프로필탭 다음사진으로
+def next():
+    return render_template('/picture.html')
+
+
+@main.route('/upload/calculate',methods=['POST']) # 업로드 계산기
 def calculate(num=None):
     if request.method == 'POST':
         temp = request.form['num']
     else:
         num=None
     return redirect(url_for('.datecal',num=temp))   # .써라
+
+
 
