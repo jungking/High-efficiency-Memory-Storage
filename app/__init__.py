dd92d84673096eb5380fb8bf776f3f20004c3c21
jungking <immenige2@gmail.com>
2021-04-11 23:10:44 +0900

    .

--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-# pwd : /python_web/app/__init.py__
-
-from flask import Flask
-
-app = Flask(__name__)
-
-from app.main.index import app as main
-app.register_blueprint(main)
---
+++ app/app.py
@@ -0,0 +1,33 @@
+
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
+from model.my_user_model import db
+
+from route.user_route import router
+
+app = Flask(__name__)
+
+app.register_blueprint(router)
+
+if __name__ == "__main__":
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    dbfile = os.path.join(basedir, 'db.sqlite')
+
+    app.config['SQLALCHEMY_DATABASE_RUI'] = 'sqlite:///' + dbfile
+    app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = 'qwerasdfzxcv'
+
+    csrf = CSRFProtect()
+    csrf.init_app(app)
+
+    db.init_app(app)
+    db.app = app
+    db.create_all()
+
+   
+    app.run(debug=True)
+    #app.run(debug=True,host="127.0.0.1",port=5000)
+
