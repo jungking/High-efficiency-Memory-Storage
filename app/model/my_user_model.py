a2228834bfdea7a6230b7673f5ae1ba054bc7bb0
jungking <immenige2@gmail.com>
2021-04-12 00:36:24 +0900

    .

--- app/model/my_user_model.py
+++ app/model/my_user_model.py
@@ -3,16 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    # Create user table
-
-    __tablename__ = 'my_user'
-
-    id = db.Column(db.Integer, primary_key=True)
-    userid = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(80), nullable=False)
+class User(db.Model): #데이터 모델을 나타내는 객체 선언
+    __tablename__ = 'user_table' #테이블 이름
     
+    id = db.Column(db.Integer, primary_key=True)
+    userid = db.Column(db.String(32), unique=True, nullable=False)
+    password = db.Column(db.String(8), nullable=False)
+
     def __init__(self, userid, password):
         self.userid = userid
         self.password = password
+
     
