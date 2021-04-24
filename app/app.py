db431f417ccfe3cc4e2aaa8943a7bca88395a818
jungking <immenige2@gmail.com>
2021-04-24 12:04:37 +0900

    .

--- app/app.py
+++ app/app.py
@@ -4,9 +4,11 @@ from flask_wtf.csrf import CSRFProtect
 from flask import Blueprint, request, Flask, session, render_template, redirect, url_for
 from app.model import my_user_model
 from . import form
+import dash
 
 app = Flask(__name__)
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP], app=app)
+
 """ db = SQLAlchemy() #SQLAlchemy를 사용해 데이터베이스 저장
 
 class User(db.Model): #데이터 모델을 나타내는 객체 선언
