6bb0b6a69e0de4c6cf693311546272cb4b9d58b6
jungking <immenige2@gmail.com>
2021-05-10 11:20:16 +0900

    .

--- app/model/my_user_model.py
+++ app/model/my_user_model.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy() #SQLAlchemy를 사용해 데이터베이스 저장
 
 class User(db.Model): #데이터 모델을 나타내는 객체 선언
-    __tablename__ = 'user_table' #테이블 이름
+    __tablename__ = 'user' #테이블 이름
     
     id = db.Column(db.Integer, primary_key=True)
     userid = db.Column(db.String(32), unique=True, nullable=False)
