d1d3c294257fbb40019e2ed3333db9eafdf88490
jungking <immenige2@gmail.com>
2021-06-04 14:45:59 +0900

    delete : config.py contents

--- cofig.py
+++
@@ -1,9 +0,0 @@
-db = {
-    'user'     : 'root',
-    'password' : 'kh12241224',
-    'host'     : '127.0.0.1',
-    'port'     : '3306',
-    'database' : 'flask_db'
-}
-
-DB_URL = f"mysql+mysqlconnector://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}?charset=utf8" 
