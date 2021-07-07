9d3be1c168d7a69b57b41273406d597d15552d25
jungking <immenige2@gmail.com>
2021-07-07 14:31:28 +0900

    heroku

--- app/app.py
+++ app/app.py
@@ -263,5 +263,5 @@ def delete():
     #subid 삭제
     return redirect('/picture')
 
-app.run(debug=True,host="0.0.0.0",port=os.environ.get("PORT",5000))
+app.run(debug=True,host="0.0.0.0",port=int(os.environ.get("PORT",5000)))
 #app.run(debug=True)
