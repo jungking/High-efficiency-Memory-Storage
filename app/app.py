fc712b925f4654ebb0ca26474bb8162bbfa37fcf
jungking <immenige2@gmail.com>
2021-07-07 14:13:18 +0900

    fix procfile

--- app/app.py
+++ app/app.py
@@ -264,4 +264,5 @@ def delete():
     return redirect('/picture')
 
 port = int(os.environ.get("PORT",5000))
-app.run(debug=True,host="127.0.0.1",port=port)
+#app.run(debug=True,host="127.0.0.1",port=port)
+app.run(debug=True)
